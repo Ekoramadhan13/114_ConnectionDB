8eb5e385bdbf40691a44074cbaa7d1e73e5d0b23
Ekoramadhan13 <enugraha411@gmail.com>
2025-10-15 12:13:23 +0700

    menambahkan koneksi ke database

--- index.js
+++ index.js
@@ -6,3 +6,19 @@ const app = express();
 app.use(express.json());
 
 
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    port: '3309', 
+    password: 'Sambeng13',
+    database: 'mahasiswa'
+});
+
+db.connect((err) => {
+    if (err) {
+        console.log(' Error connection to MySQL:', err.stack);
+        return;
+    }
+    console.log(' MySQL connected successfully');
+});
+
